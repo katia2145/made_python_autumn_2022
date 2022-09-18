e076525ec45de21b3eeff7cde64099bf2f4c8879
katia2145 <74782464+katia2145@users.noreply.github.com>
2022-09-18 19:38:14 +0300

    Update homework-01.py

--- 01/homework-01.py
+++ 01/homework-01.py
@@ -1,24 +1,25 @@
 import math
-
+eps = 1e-10
 
 def roots_of_quadratic_equation(a, b, c):
-    if (not isinstance(a, int) or
-        not isinstance(b, int) or
-        not isinstance(c, int)):
+    if (not (isinstance(a, int) or isinstance(a, float)) or
+        not (isinstance(b, int) or isinstance(a, float)) or
+        not (isinstance(c, int) or isinstance(a, float))):
         return None
     
-    if a == 0:
+    if math.isclose(a, 0):
         return None
     
     D = b**2 - 4*a*c
     
-    if D > 0:
+    if math.isclose(D, 0):
+        x1 = -b / 2*a
+        return x1
+    
+    if D > -eps:
         x1 = (-b + math.sqrt(D)) / 2*a
         x2 = (-b - math.sqrt(D)) / 2*a
         return x1, x2
-    if D == 0:
-        x1 = -b / 2*a
-        return x1
     
     D *= -1
     x1 = complex(-b, math.sqrt(D)) / 2*a
@@ -47,6 +48,7 @@ assert roots_of_quadratic_equation('2', 1, 2) == None
 assert roots_of_quadratic_equation(2, '1', 2) == None
 assert roots_of_quadratic_equation(2, 1, '2') == None
 assert roots_of_quadratic_equation(1, -2, -3) == (3.0, -1.0)
+assert roots_of_quadratic_equation(1.0, -2.0, -3.0) == (3.0, -1.0)
 assert roots_of_quadratic_equation(16, 8, 1)  == -64.0
 assert roots_of_quadratic_equation(2, 4, 4)   == ((-4+4j), (-4-4j))
 
