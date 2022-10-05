cdd74030c9a71cc742a4654143ed6dc0babd93c7
katia2145 <74782464+katia2145@users.noreply.github.com>
2022-10-05 09:33:42 +0300

    Update CustomList.py

--- 04/CustomList.py
+++ 04/CustomList.py
@@ -17,21 +17,7 @@ class CustomList(list):
         return CustomList(new_lst)
 
     def __radd__(self, lst):
-        if not isinstance(lst, list) and not isinstance(lst, CustomList):
-            raise TypeError("can only add list or CustomList to CustomList")
-
-        new_lst = []
-        max_len = max(len(self), len(lst))
-
-        for i in range(max_len):
-            if i >= len(self):
-                new_lst.append(lst[i])
-            elif i >= len(lst):
-                new_lst.append(self[i])
-            else:
-                new_lst.append(self[i] + lst[i])
-
-        return CustomList(new_lst)
+        return self + lst
 
     def __sub__(self, lst):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
@@ -71,45 +57,37 @@ class CustomList(list):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
             raise TypeError("can only compare CustomList with CustomList")
 
-        return True if sum(self) < sum(lst) else False
+        return sum(self) < sum(lst)
 
     def __gt__(self, lst):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
             raise TypeError("can only compare CustomList with CustomList")
 
-        return True if sum(self) > sum(lst) else False
+        return sum(self) > sum(lst)
 
     def __le__(self, lst):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
             raise TypeError("can only compare CustomList with CustomList")
 
-        return True if sum(self) <= sum(lst) else False
+        return sum(self) <= sum(lst)
 
     def __ge__(self, lst):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
             raise TypeError("can only compare CustomList with CustomList")
 
-        return True if sum(self) >= sum(lst) else False
+        return sum(self) >= sum(lst)
 
     def __eq__(self, lst):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
             raise TypeError("can only compare CustomList with CustomList")
 
-        return True if sum(self) == sum(lst) else False
+        return sum(self) == sum(lst)
 
     def __ne__(self, lst):
         if not isinstance(lst, list) and not isinstance(lst, CustomList):
             raise TypeError("can only compare CustomList with CustomList")
 
-        return True if sum(self) != sum(lst) else False
+        return sum(self) != sum(lst)
 
     def __str__(self):
-        s = "CustomList = ["
-        for i in range(len(self)):
-            if i == len(self) - 1:
-                s += str(self[i]) + "]"
-            else:
-                s += str(self[i]) + ", "
-
-        s += "\nsum = " + str(sum(self))
-        return s
+        return f'CustomList({list(self)})\nsum={sum(self)}'
